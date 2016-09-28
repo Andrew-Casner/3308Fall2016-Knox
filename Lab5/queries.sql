d3973c55f20c1696b1b6c8e5e8ed69dd129389d3
andrew-casner <a.casner@comcast.net>
2016-09-28 14:49:59 -0600

    Next few queries

--- Lab5/queries.sql
+++ Lab5/queries.sql
@@ -1,2 +1,8 @@
-SELECT Name, Price FROM store WHERE Price < 1.0
+SELECT Name, Price FROM store WHERE Price <= 1.0
+SELECT Name, Qty * Price From store 
+SELECT SUM(Qty * Price) FROM store
+SELECT COUNT(Id) FROM store
+SELECT Name FROM Course
+
+
 
