e37fe70dbba012ffc92266876773dbf03ffd3c5e
andrew-casner <a.casner@comcast.net>
2016-09-28 15:32:22 -0600

    Finished Lab

--- Lab5/queries.sql
+++ Lab5/queries.sql
@@ -2,10 +2,10 @@ SELECT Name, Price FROM store WHERE Price <= 1.0
 SELECT Name, Qty * Price From store 
 SELECT SUM(Qty * Price) FROM store
 SELECT COUNT(Id) FROM store
-SELECT Name FROM Course
+SELECT DISTINCT Name FROM Course
 SELECT SUM(count) FROM Enrollment
-SELECT  COUNT(Id) FROM Course
+SELECT  COUNT(DISTINCT Name) FROM Course
 SELECT COUNT(Id) FROM Dept
-SELECT deptId, Name FROM Course, Dept WHERE Course.deptId = Dept.Name
-SELECT deptId, Name FROM Course, Dept WHERE Course.deptID = Dept.Name AND Dept.Name = 'CSC'  
-SELECT deptId, Name, Enrollment FROM Course.deptId = Dept.Name 
+SELECT deptId, Course.Name FROM Course, Dept WHERE Course.deptId = Dept.Name
+SELECT deptId, Course.Name FROM Course, Dept WHERE Course.deptID = Dept.Name AND Dept.Name = 'CSC'  
+SELECT deptId, Course.Name, Enrollment FROM Course.deptId = Dept.Name 
